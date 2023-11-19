067e0a8c23217c7ec2d8613287bf978f394422cc
DanielLisjak <94532105+DanielLisj@users.noreply.github.com>
2023-11-19 21:58:31 +0100

    2 Commit

---
+++ src/main/java/Model/GrammarTrainer.java
@@ -0,0 +1,132 @@
+package Model;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Diese Klasse verwaltet die Wordpairs
+ * @author Daniel Lisjak
+ */
+public class GrammarTrainer {
+
+    private List<WordPair> pair;
+    private int wrong;
+    private int right;
+    private String input;
+    private int pairIndex=0;
+
+    public GrammarTrainer() {
+        this.pair = new LinkedList<>();
+    }
+
+    /**
+     * Getter methode für richtig eingegebene wörter
+     * @return anzahl
+     */
+    public int getRight() {
+        return this.right;
+    }
+
+    /**
+     * Getter methode für falsch eingegebene wörter
+     * @return anzahl
+     */
+    public int getWrong() {
+        return this.wrong;
+    }
+
+    /**
+     * Gibt den index für das aktuell angezeigte pair an
+     * @return index des pairs
+     */
+    public int getPairIndex()	{
+        return this.pairIndex;
+    }
+
+    /**
+     * Fügt ein wordpair in die Liste hinzu
+     * @param pair ein Wordpair welches hinzugefügt werden will
+     */
+    public void addWordpair(WordPair pair) {
+        if(pair!=null)	{
+            this.pair.add(pair);
+        } else {
+            throw new IllegalArgumentException("Wordpair is null");
+        }
+    }
+
+    /**
+     * Gibt das Wordpair an einem angegebenen Index zurück
+     * @param index der gewollte index
+     * @return Das wordpair an der Stelle des Index
+     */
+    public WordPair getWordpair(int index) {
+        if(index <= pair.size()-1)	{
+            return this.pair.get(index);
+        } else {
+            throw new IllegalArgumentException("Index doesn't exist");
+        }
+    }
+
+    /**
+     * Gibt ein zufällig ausgewähltes Wordpair zurück
+     * @return das zufällig ausgewählte Wordpar
+     */
+    public WordPair getRandomPair()	{
+        Random rand = new Random();
+        int rnum = rand.nextInt((this.pair.size()-1)+1)+0;
+        this.pairIndex=rnum;
+        return this.pair.get(rnum);
+    }
+
+    /**
+     * Gibt die Liste an Wordpairs zurück
+     * @return die Wordpairliste
+     */
+    public List<WordPair> getPairList()	{
+        return this.pair;
+    }
+
+    /**
+     * Setzt den eingegebenen Text
+     * @param input der eingegebene Text
+     */
+    public void setInput(String input) {
+        if(!input.equals(null))	{
+            this.input = input;
+        }
+    }
+
+    /**
+     * Setzt die aktuell richtigen wörter
+     * @param right anzahl
+     */
+    public void setRight(int right)	{
+        this.right = right;
+    }
+
+    /**
+     * Setzt die aktull falschen wörter
+     * @param wrong anzahl
+     */
+    public void setWrong(int wrong)	{
+        this.wrong = wrong;
+    }
+
+    /**
+     * Vergleicht den eingegebenen Text mit dem Wordpair
+     * @param current zu überorüfendes Wordpair
+     * @return ob es richtig oder falsch
+     */
+    public boolean checkInput(WordPair current)	{
+        if(this.input.equals(current.getWord()))	{
+            this.right++;
+            return true;
+        } else {
+            this.wrong++;
+            return false;
+        }
+    }
+
+}
---
+++ src/main/java/Model/WordPair.java
@@ -0,0 +1,64 @@
+package Model;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Diese Klasse enthält die Wortpaare auf die in dem Programm geprüft werden.
+ * @author Daniel Lisjak
+ */
+public class WordPair {
+
+    private String word;
+    private String url;
+
+    /**
+     * Konstruktor
+     * @param word Das Wort welches das Bild beschreibt
+     * @param url Das Bild, das zumm Wort passt
+     */
+    public WordPair(String word, String url) {
+        this.url = url;
+        this.word = word;
+    }
+
+    /**
+     * Setter Methode für das Attribut word
+     * @param word Das Wort welches das Bild beschreibt
+     */
+    public void setWord(String word) {
+        if(!word.equals(null) && !word.equals("")){
+            this.word = word;
+        }
+    }
+
+    /**
+     * Setter Methode für das Attribut url
+     * @param url Das Bild, das zum Wort passt
+     */
+    public void setURL(String url) {
+        try{
+            URL u = new URL(url);
+            this.url = url;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Gibt das Wort zurück
+     * @return Das Wort
+     */
+    public String getWord() {
+        return this.word;
+    }
+
+    /**
+     * Gibt die URL zurück
+     * @return Die URL
+     */
+    public String getURL() {
+        return this.url;
+    }
+
+}
