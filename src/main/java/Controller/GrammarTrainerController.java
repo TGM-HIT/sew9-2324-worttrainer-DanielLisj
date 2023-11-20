657f5316995e636ef027a19d57fe09ec54ff8e32
DanielLisjak <94532105+DanielLisj@users.noreply.github.com>
2023-11-20 12:52:30 +0100

    4 Commit Error gefixed, UML erstellt

--- src/main/java/Controller/GrammarTrainerController.java
+++ src/main/java/Controller/GrammarTrainerController.java
@@ -1,4 +1,17 @@
 package Controller;
 
-public class GrammarTrainerController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Diese Klasse enthält die Logik für das Programm
+ * @author Daniel Lisjak
+ */
+public class GrammarTrainerController implements ActionListener {
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
