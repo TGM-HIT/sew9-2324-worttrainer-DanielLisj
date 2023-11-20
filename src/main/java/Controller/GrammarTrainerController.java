8a1637440b95aa9b4de5a33a2907049fe5ee7880
DanielLisjak <94532105+DanielLisj@users.noreply.github.com>
2023-11-20 22:51:09 +0100

    9 Commit Controller fertig

--- src/main/java/Controller/GrammarTrainerController.java
+++ src/main/java/Controller/GrammarTrainerController.java
@@ -20,27 +20,60 @@ public class GrammarTrainerController implements ActionListener {
 
     public GrammarTrainerController() {
         this.panel = new GrammarTrainerPanel(this);
-        this.frame = new GrammarTrainerFrame("Worttrainer",this.panel);
+        this.frame = new GrammarTrainerFrame("Worttrainer", this.panel);
         this.save = new GrammarTrainerSave();
         this.grammarTrainer = save.load("GrammarTrainer.txt");
     }
+
+    public static void main(String[] args) {
+        GrammarTrainerController trainer = new GrammarTrainerController();
+        trainer.panel.setUrl(trainer.grammarTrainer.getRandomPair().getURL());
+        trainer.panel.setRight(trainer.grammarTrainer.getRight() + "");
+        trainer.panel.setWrong(trainer.grammarTrainer.getWrong() + "");
+        trainer.panel.setCounter(trainer.grammarTrainer.getWrong() + trainer.grammarTrainer.getRight() + "");
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getActionCommand().equals("end"))	{
+        if (e.getActionCommand().equals("end")) {
             this.panel.inputEnable(false);
             this.panel.enableReset(false);
             this.panel.enableEnd(false);
             this.panel.endMessage();
-            this.save.save("GrammarTrainer.txt",this.grammarTrainer);
+            this.save.save("GrammarTrainer.txt", this.grammarTrainer);
         }
 
         // Dieser Teil trifft ein, wenn der zurücksetzen button gedrückt wird
-        if(e.getActionCommand().equals("reset"))	{
+        if (e.getActionCommand().equals("reset")) {
             this.grammarTrainer.setRight(0);
             this.grammarTrainer.setWrong(0);
             this.panel.setRight("0");
             this.panel.setWrong("0");
             this.panel.setCounter("0");
+        }
+        // Dieser Teil trifft ein, wenn der zurücksetzen button gedrückt wird
+        if (e.getActionCommand().equals("reset")) {
+            this.grammarTrainer.setRight(0);
+            this.grammarTrainer.setWrong(0);
+            this.panel.setRight("0");
+            this.panel.setWrong("0");
+            this.panel.setCounter("0");
+        }
+
+        // Dieser Teil trifft ein, wenn der Benutzer text eingegeben und enter gedrückt hat
+        if (e.getActionCommand().equals("input")) {
+            if (this.grammarTrainer.checkInput(grammarTrainer.getWordpair(grammarTrainer.getPairIndex()))) {
+                this.panel.setRight(grammarTrainer.getRight() + "");
+                this.panel.correctMessage();
+                this.panel.setCounter(grammarTrainer.getRight() + grammarTrainer.getWrong() + "");
+                this.panel.resetField();
+                this.panel.setUrl(grammarTrainer.getRandomPair().getURL());
+            } else {
+                this.panel.setWrong(grammarTrainer.getWrong() + "");
+                this.panel.wrongMessage();
+                this.panel.resetField();
+                this.panel.setCounter(grammarTrainer.getWrong() + grammarTrainer.getRight() + "");
+            }
         }
     }
 }
